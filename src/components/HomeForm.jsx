d0abc900a2cbd80af3bd9fcb3bb69eb2b503ffb5
pabitra3362 <pmohanty1435@gmail.com>
2024-12-16 13:49:43 +0530

    form problem solve

--- src/components/HomeForm.jsx
+++ src/components/HomeForm.jsx
@@ -1,21 +1,20 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import vdo from "../assets/Taxi booking.mp4";
 import { motion } from "framer-motion";
-import Input from './Input';
-import { FaUser } from "react-icons/fa";
-import { FaPhone } from "react-icons/fa";
+import Input from "./Input";
+import { FaUser, FaPhone } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { BsCalendarDateFill } from "react-icons/bs";
 import Button from "./Button";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 
-const HomeForm = ({ ref2 }) => {
+const HomeForm = () => {
   const [selectedClass, setSelectedClass] = useState("economy");
   const [address1, setAddress1] = useState(""); // Starting point
   const [address2, setAddress2] = useState(""); // Ending point
-  const [suggestions1, setSuggestions1] = useState([]); // Suggestions for starting point
-  const [suggestions2, setSuggestions2] = useState([]); // Suggestions for ending point
+  const [suggestions1, setSuggestions1] = useState([]);
+  const [suggestions2, setSuggestions2] = useState([]);
 
   const {
     register,
@@ -25,7 +24,7 @@ const HomeForm = ({ ref2 }) => {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
+    console.log({ ...data, selectedClass });
   };
 
   const handleChange = async (event, field) => {
@@ -39,14 +38,17 @@ const HomeForm = ({ ref2 }) => {
 
     if (query) {
       try {
-        const response = await axios.get("https://nominatim.openstreetmap.org/search", {
-          params: {
-            q: query,
-            format: "json",
-            addressdetails: 1,
-            limit: 5,
-          },
-        });
+        const response = await axios.get(
+          "https://nominatim.openstreetmap.org/search",
+          {
+            params: {
+              q: query,
+              format: "json",
+              addressdetails: 1,
+              limit: 5,
+            },
+          }
+        );
 
         if (field === "startingPoint") {
           setSuggestions1(response.data);
@@ -79,38 +81,27 @@ const HomeForm = ({ ref2 }) => {
   const containerVariants = {
     hidden: {
       opacity: 0,
-      transition: {
-        staggerChildren: 0.2,
-      },
+      transition: { staggerChildren: 0.2 },
     },
     visible: {
       opacity: 1,
-      transition: {
-        staggerChildren: 0.2,
-      },
+      transition: { staggerChildren: 0.2 },
     },
   };
 
   const itemVariants = {
-    hidden: {
-      y: 20,
-      opacity: 0,
-    },
+    hidden: { y: 20, opacity: 0 },
     visible: {
       y: 0,
       opacity: 1,
-      transition: {
-        type: "spring",
-        stiffness: 100,
-      },
+      transition: { type: "spring", stiffness: 100 },
     },
   };
 
   return (
     <div>
-      {/* Booking form */}
       <div className="w-full bg-slate-200 py-1 md:py-5 lg:py-16">
-        <div className="w-full md:w-[80vw] lg:w-[70vw] mx-auto px-3 py-7 md:py-10 overflow-hidden grid md:flex justify-items-center md:justify-between items-center gap-8 lg:gap-2 bg-white rounded-lg">
+        <div className="w-full md:w-[80vw] lg:w-[70vw] mx-auto px-3 py-7 md:py-10 overflow-hidden grid md:flex justify-items-center md:justify-between items-center gap-8 lg:gap-2 bg-white rounded-lg hover:shadow-lg transition duration-200">
           <div className="left bg-custom-yellow w-80 h-96 md:h-[40vh] lg:w-[35vw] lg:h-[50vh] flex justify-center items-center relative">
             <video
               className="absolute top-0 left-0 w-full h-full object-cover"
@@ -120,69 +111,38 @@ const HomeForm = ({ ref2 }) => {
             ></video>
           </div>
           <div className="right grid items-center gap-5">
-            <h2 className="font-bold text-black text-3xl">Booking Taxi Online</h2>
+            <h2 className="font-bold text-black text-3xl">
+              Booking Taxi Online
+            </h2>
             <div className="flex justify-start items-center gap-5">
-              <div
-                onClick={() => setSelectedClass("standard")}
-                className={`${
-                  selectedClass === "standard"
-                    ? "border-b-2 border-yellow-400 text-yellow-400"
-                    : "text-black"
-                } transition duration-200 font-bold text text-sm cursor-pointer`}
-              >
-                STANDARD
-              </div>
-              <div
-                onClick={() => setSelectedClass("economy")}
-                className={`${
-                  selectedClass === "economy"
-                    ? "border-b-2 border-yellow-400 text-yellow-400"
-                    : "text-black"
-                } transition duration-200 font-bold text text-sm cursor-pointer`}
-              >
-                ECONOMY
-              </div>
-              <div
-                onClick={() => setSelectedClass("business")}
-                className={`${
-                  selectedClass === "business"
-                    ? "border-b-2 border-yellow-400 text-yellow-400"
-                    : "text-black"
-                } transition duration-200 font-bold text text-sm cursor-pointer`}
-              >
-                BUSINESS
-              </div>
-              <div
-                onClick={() => setSelectedClass("vip")}
-                className={`${
-                  selectedClass === "vip"
-                    ? "border-b-2 border-yellow-400 text-yellow-400"
-                    : "text-black"
-                } transition duration-200 font-bold text text-sm cursor-pointer`}
-              >
-                VIP
-              </div>
+              {["standard", "economy", "business", "vip"].map((cls) => (
+                <div
+                  key={cls}
+                  onClick={() => setSelectedClass(cls)}
+                  className={`${
+                    selectedClass === cls
+                      ? "border-b-2 border-yellow-400 text-yellow-400"
+                      : "text-black"
+                  } transition duration-200 font-bold text-sm cursor-pointer`}
+                >
+                  {cls.toUpperCase()}
+                </div>
+              ))}
             </div>
             <motion.form
-              ref={ref2}
               onSubmit={handleSubmit(onSubmit)}
               variants={containerVariants}
               initial="hidden"
               whileInView="visible"
               className="grid justify-items-center items-center w-full gap-3"
             >
-              {/* name */}
-              <motion.div className="name" variants={itemVariants}>
+              {/* Name */}
+              <motion.div variants={itemVariants}>
                 <Input
                   type="text"
                   placeholder="Your Name"
                   icon={<FaUser />}
-                  {...register("name", {
-                    required: {
-                      value: true,
-                      message: "Name is required",
-                    },
-                  })}
+                  {...register("name", { required: "Name is required" })}
                 />
                 {errors.name && (
                   <span className="text-red-500 my-2 font-bold">
@@ -191,19 +151,16 @@ const HomeForm = ({ ref2 }) => {
                 )}
               </motion.div>
 
-              {/* phone */}
-              <motion.div className="phone" variants={itemVariants}>
+              {/* Phone */}
+              <motion.div variants={itemVariants}>
                 <Input
                   type="number"
                   placeholder="Phone"
                   icon={<FaPhone />}
                   {...register("phone", {
-                    required: {
-                      value: true,
-                      message: "Phone number is required",
-                    },
+                    required: "Phone number is required",
                     pattern: {
-                      value: /^[0-9]{10}$/g,
+                      value: /^[0-9]{10}$/,
                       message: "Enter a valid phone number",
                     },
                   })}
@@ -215,8 +172,8 @@ const HomeForm = ({ ref2 }) => {
                 )}
               </motion.div>
 
-              {/* start-destination */}
-              <motion.div className="start-destination" variants={itemVariants}>
+              {/* Start Destination */}
+              <motion.div variants={itemVariants}>
                 <Controller
                   name="startingPoint"
                   control={control}
@@ -228,18 +185,26 @@ const HomeForm = ({ ref2 }) => {
                         type="text"
                         placeholder="Start Destination"
                         icon={<FaLocationDot />}
-                        onChange={(e) => handleChange(e, "startingPoint")}
-                        value={address1}
+                        onChange={(e) => {
+                          field.onChange(e.target.value); // Update react-hook-form's state
+                          handleChange(e, "startingPoint"); // Update suggestions and local state
+                        }}
+                        value={address1} // Use local state to display value
                       />
-                      {suggestions1.length > 0 &&
-                        suggestions1.map((item, index) => (
-                          <div className="my-2 w-80 lg:w-[35vw]"
-                            key={index}
-                            onClick={(e) => handleSelect(e, "startingPoint")}
-                          >
-                            {item.display_name}
-                          </div>
-                        ))}
+                      <div className="max-h-20 overflow-hidden overflow-y-scroll">
+                      {suggestions1.map((item, index) => (
+                        <div
+                          key={index}
+                          className="cursor-pointer w-80 lg:w-[35vw]"
+                          onClick={(e) => {
+                            handleSelect(e, "startingPoint");
+                            field.onChange(e.target.textContent); // Update react-hook-form's state
+                          }}
+                        >
+                          {item.display_name}
+                        </div>
+                      ))}
+                      </div>
                       {errors.startingPoint && (
                         <span className="text-red-500 my-2 font-bold">
                           {errors.startingPoint.message}
@@ -250,8 +215,8 @@ const HomeForm = ({ ref2 }) => {
                 />
               </motion.div>
 
-              {/* end-destination */}
-              <motion.div className="end-destination" variants={itemVariants}>
+              {/* End Destination */}
+              <motion.div variants={itemVariants}>
                 <Controller
                   name="endingPoint"
                   control={control}
@@ -263,18 +228,24 @@ const HomeForm = ({ ref2 }) => {
                         type="text"
                         placeholder="End Destination"
                         icon={<FaLocationDot />}
-                        onChange={(e) => handleChange(e, "endingPoint")}
-                        value={address2}
+                        onChange={(e) => {
+                          field.onChange(e.target.value); // Update react-hook-form's state
+                          handleChange(e, "endingPoint"); // Update suggestions and local state
+                        }}
+                        value={address2} // Use local state to display value
                       />
-                      {suggestions2.length > 0 &&
-                        suggestions2.map((item, index) => (
-                          <div className="w-80 lg:w-[35vw]"
-                            key={index}
-                            onClick={(e) => handleSelect(e, "endingPoint")}
-                          >
-                            {item.display_name}
-                          </div>
-                        ))}
+                      {suggestions2.map((item, index) => (
+                        <div
+                          key={index}
+                          className="cursor-pointer w-80 lg:w-[35vw]"
+                          onClick={(e) => {
+                            handleSelect(e, "endingPoint");
+                            field.onChange(e.target.textContent); // Update react-hook-form's state
+                          }}
+                        >
+                          {item.display_name}
+                        </div>
+                      ))}
                       {errors.endingPoint && (
                         <span className="text-red-500 my-2 font-bold">
                           {errors.endingPoint.message}
@@ -285,18 +256,12 @@ const HomeForm = ({ ref2 }) => {
                 />
               </motion.div>
 
-              {/* date */}
-              <motion.div className="date" variants={itemVariants}>
+              {/* Date */}
+              <motion.div variants={itemVariants}>
                 <Input
                   type="date"
-                  placeholder="Date"
                   icon={<BsCalendarDateFill />}
-                  {...register("date", {
-                    required: {
-                      value: true,
-                      message: "Date is required",
-                    },
-                  })}
+                  {...register("date", { required: "Date is required" })}
                 />
                 {errors.date && (
                   <span className="text-red-500 my-2 font-bold">
@@ -305,8 +270,12 @@ const HomeForm = ({ ref2 }) => {
                 )}
               </motion.div>
 
-              <motion.div className="book-btn w-80 lg:w-[35vw]" variants={itemVariants}>
-                <Button type="submit" className="w-full py-3 border border-black rounded-lg">
+              {/* Submit Button */}
+              <motion.div className="w-80 lg:w-[35vw]" variants={itemVariants}>
+                <Button
+                  type="submit"
+                  className="w-full py-3 border border-black rounded-lg"
+                >
                   Book Now!
                 </Button>
               </motion.div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Hero from "../components/Hero";
 import HomeForm from "../components/HomeForm";
 
 const Home = () => {
-  const ref=useRef(null)
   
   return (
     <div>
@@ -11,7 +10,7 @@ const Home = () => {
       <Hero />
 
       {/* Booking form */}
-      <HomeForm ref2={ref} />
+      <HomeForm />
       
     </div>
   );
