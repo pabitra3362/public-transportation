381073406792485a6cb24c5a88a1b1fcc5f40e52
sehzad-Ansari <roysehzad526@gmail.com>
2024-12-19 08:51:07 +0530

    half of about  page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import AboutHero from '../components/AboutHero'
+import Team from './Team'
 
 
 const About = () => {
   return (
     <div>
       <AboutHero />
-      
-
+      <Team />
     </div>
   )
 }
