4ece3501f68764289f52e23794dae95eba2aff9a
pabitra3362 <pmohanty1435@gmail.com>
2024-12-19 15:18:09 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-/* eslint-disable no-undef */
 import flowbite from "flowbite-react/tailwind";
-// eslint-disable-next-line no-unused-vars
-import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+  ],
   theme: {
     extend: {
       colors:{
@@ -16,8 +17,11 @@ export default {
       }
     },
   },
-  plugins: [require("daisyui"), flowbite.plugin(daisyui)],
+  plugins: [
+    require('daisyui'),
+    flowbite.plugin(),
+  ],
   daisyui: {
     themes: ["light"],
   },
-};
+}
