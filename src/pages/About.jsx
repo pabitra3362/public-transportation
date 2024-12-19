b32fdbc9b35a61dd0e0c39d2765de97ceb6951a4
sehzad-Ansari <roysehzad526@gmail.com>
2024-12-19 08:50:50 +0530

    half of about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,15 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import AboutHero from '../components/AboutHero'
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <AboutHero />
+      
+
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,20 @@
+/* eslint-disable no-undef */
 import flowbite from "flowbite-react/tailwind";
+// eslint-disable-next-line no-unused-vars
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {
-      colors:{
-        "custom-yellow": "rgba(255, 250, 0, 0.8)"
-      }
+      colors: {
+        "custom-yellow": "rgba(255, 250, 0, 0.8)",
+      },
     },
   },
-  plugins: [
-    require('daisyui'),
-    flowbite.plugin(),
-  ],
+  plugins: [require("daisyui"), flowbite.plugin(daisyui)],
   daisyui: {
     themes: ["light"],
   },
-}
+};
