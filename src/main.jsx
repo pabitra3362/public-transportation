79a7359787c92ed7858c514e02572d63876a1130
pabitra3362 <pmohanty1435@gmail.com>
2025-01-01 20:01:47 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
