dd03d4fb7c8f809722afe85642a2c35250378d39
sehzad-Ansari <roysehzad526@gmail.com>
2025-01-17 09:36:47 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import News from "./pages/News";
 import Contacts from "./pages/Contacts";
 import Footer from "./components/Footer";
 import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
           <Route path="/news" element={<News />} />
           <Route path="/contact" element={<Contacts />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </Router>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -44,6 +44,10 @@ const SignUp = () => {
     navigate("/");
   };
 
+  const navigateToLogin = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="relative flex flex-col md:flex-row bg-white rounded-lg shadow-md overflow-hidden w-full max-w-4xl">
@@ -61,7 +65,7 @@ const SignUp = () => {
           {/* Close Button */}
           <button
             onClick={handleClose}
-            className="absolute top-[-1px] right-0 text-[20px] p-2 rounded-full text-black"
+            className="absolute top-[1px] right-0 text-[15px] w-[40px] h-[40px] p-2  rounded-full  text-black hover:text-yellow-300 hover:bg-black hover:border-yellow-300"
           >
             ✕
           </button>
@@ -72,52 +76,127 @@ const SignUp = () => {
               <label className="block text-sm font-medium text-gray-700 mb-2">
                 Name
               </label>
-              <input
-                type="text"
-                name="name"
-                placeholder="Enter your Name"
-                value={formData.name}
-                onChange={handleChange}
-                className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-              />
+              <div className="nameicon flex relative">
+                <svg
+                  height={60}
+                  viewBox="0 -9 32 32"
+                  width={40}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute left-3 top-1/2 transform -translate-y-1/2 pointer-events-none"
+                >
+                  <g id="Layer_3" data-name="Layer 3">
+                    <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z" />
+                  </g>
+                </svg>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Enter your Name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-full border border-gray-300 rounded-lg pl-14 px-5 focus:ring-5 focus:ring-blue-500 focus:outline-none"
+                />
+              </div>
             </div>
             <div className="mb-4">
               <label className="block text-sm font-medium text-gray-700 mb-2">
                 Email
               </label>
-              <input
-                type="email"
-                name="email"
-                placeholder="Enter your Email"
-                value={formData.email}
-                onChange={handleChange}
-                className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-              />
+              <div className="relative">
+                <svg
+                  height={20}
+                  viewBox="0 0 32 32"
+                  width={20}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute left-3 top-1/2 transform -translate-y-1/2 pointer-events-none"
+                >
+                  <g id="Layer_3" data-name="Layer 3">
+                    <path d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z" />
+                  </g>
+                </svg>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter your Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full border border-gray-300 rounded-lg pl-10 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                />
+              </div>
             </div>
+
             <div className="mb-4">
               <label className="block text-sm font-medium text-gray-700 mb-2">
                 Password
               </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="Enter your Password"
-                value={formData.password}
-                onChange={handleChange}
-                className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-              />
-              {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+              <div className="relative">
+                <svg
+                  height={20}
+                  viewBox="-64 0 512 512"
+                  width={20}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute left-3 top-1/2 transform -translate-y-1/2 pointer-events-none"
+                >
+                  <path d="m336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0" />
+                  <path d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0" />
+                </svg>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Enter your Password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="w-full border border-gray-300 rounded-lg pl-10 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                />
+                {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+              </div>
             </div>
+
+            <p className="text-center text-sm mt-4">
+              Already have an account?{" "}
+              <span
+                className="text-yellow-300 cursor-pointer"
+                onClick={navigateToLogin}
+              >
+                Login
+              </span>
+            </p>
             <button
               type="submit"
-              className="w-full bg-yellow-300 text-black py-2 rounded-lg"
+              className="w-full bg-yellow-300  hover:bg-black hover:text-white duration-500 text-black py-2 rounded-lg mt-4"
             >
               Sign Up
             </button>
-            <p className="text-center text-sm mt-4">
-              Already have an account?{" "}
-              <span className="text-yellow-300 cursor-pointer">Login</span>
-            </p>
+
+            <div className="googlebtn mt-4">
+              <button className="flex items-center justify-center gap-2 w-full py-2 border bg-yellow-300 rounded-lg  hover:bg-black hover:text-white duration-500 ">
+                {/* Google Icon */}
+                <svg
+                  version="1.1"
+                  width={20}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    d="M113.47,309.408L95.648,375.94l-65.139,1.378C11.042,341.211,0,299.9,0,256c0-42.451,10.324-82.483,28.624-117.732h0.014l57.992,10.632l25.404,57.644c-5.317,15.501-8.215,32.141-8.215,49.456C103.821,274.792,107.225,292.797,113.47,309.408z"
+                    fill="#FBBB00"
+                  />
+                  <path
+                    d="M507.527,208.176C510.467,223.662,512,239.655,512,256c0,18.328-1.927,36.206-5.598,53.451c-12.462,58.683-45.025,109.925-90.134,146.187l-0.014-0.014l-73.044-3.727l-10.338-64.535c29.932-17.554,53.324-45.025,65.646-77.911h-136.89V208.176h138.887L507.527,208.176L507.527,208.176z"
+                    fill="#518EF8"
+                  />
+                  <path
+                    d="M416.253,455.624l0.014,0.014C372.396,490.901,316.666,512,256,512c-97.491,0-182.252-54.491-225.491-134.681l82.961-67.91c21.619,57.698,77.278,98.771,142.53,98.771c28.047,0,54.323-7.582,76.87-20.818L416.253,455.624z"
+                    fill="#28B446"
+                  />
+                  <path
+                    d="M419.404,58.936l-82.933,67.896c-23.335-14.586-50.919-23.012-80.471-23.012c-66.729,0-123.429,42.957-143.965,102.724l-83.397-68.276h-0.014C71.23,56.123,157.06,0,256,0C318.115,0,375.068,22.126,419.404,58.936z"
+                    fill="#F14336"
+                  />
+                </svg>
+                Continue With Google
+              </button>
+            </div>
           </form>
         </div>
       </div>
